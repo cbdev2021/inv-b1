8497547d37c9a4dfa7ecc37a12a79f8accfc7971
carlos <cbdev2021@gmail.com>
2023-12-06 02:32:21 -0300

    id generate endpoint

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -5,6 +5,7 @@ import {
     deleteInvoice,
     getInvoice,
     getInvoicesByUserId,
+    generateId
 } from '../controllers/invoiceController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -16,5 +17,6 @@ router.put('/update-invoice/:id', protect, updateInvoice);
 router.delete('/delete-invoice/:id', protect, deleteInvoice);
 router.get('/get-invoice/:id', protect, getInvoice);
 router.get('/get-invoices/:idUsuario', protect, getInvoicesByUserId);
+router.get('/generate-id/:invoiceType', protect, generateId);
 
 export default router;
